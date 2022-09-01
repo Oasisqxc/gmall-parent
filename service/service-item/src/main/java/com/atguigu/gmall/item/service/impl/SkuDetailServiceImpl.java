0f20334a70839a4c403169cbcfa7ea07fa509c19
Oasisqxc <qxc3070601292@gmail.com>
2022-09-01 21:01:23 +0800

    day06异步编排改造远程业务完成

--- service/service-item/src/main/java/com/atguigu/gmall/item/service/impl/SkuDetailServiceImpl.java
+++ service/service-item/src/main/java/com/atguigu/gmall/item/service/impl/SkuDetailServiceImpl.java
@@ -13,43 +13,69 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Service
 public class SkuDetailServiceImpl implements SkuDetailService {
 
+
     @Autowired
     SkuDetailFeignClient skuDetailFeignClient;
 
+    @Autowired
+    ThreadPoolExecutor executor;//可配置的线程池，可自动注入
+
     @Override
     public SkuDetailTo getSkuDetail(Long skuId) {
+
         SkuDetailTo detailTo = new SkuDetailTo();
+
+        CompletableFuture<SkuInfo> skuInfoFuture = CompletableFuture.supplyAsync(() -> {
+//        1.查询基本信息
+            Result<SkuInfo> result = skuDetailFeignClient.getSkuInfo(skuId);
+            SkuInfo skuInfo = result.getData();
+            detailTo.setSkuInfo(skuInfo);
+            return skuInfo;
+        }, executor);
 //远程调用商品服务
 //        Result<SkuDetailTo> skuDetail = skuDetailFeignClient.getDetail(skuId);
-//        1.查询基本信息
-        Result<SkuInfo> result = skuDetailFeignClient.getSkuInfo(skuId);
-        SkuInfo skuInfo = result.getData();
-        detailTo.setSkuInfo(skuInfo);
 
-//        2.查询商品图片信息
-        Result<List<SkuImage>> skuImages = skuDetailFeignClient.getSkuImages(skuId);
-        skuInfo.setSkuImageList(skuImages.getData());
-//        3.查询销售属性名和值
+        //        2.查询商品图片信息
+        CompletableFuture<Void> imageFuture = skuInfoFuture.thenAcceptAsync(skuInfo -> {
+            Result<List<SkuImage>> skuImages = skuDetailFeignClient.getSkuImages(skuId);
+            skuInfo.setSkuImageList(skuImages.getData());
+        }, executor);
 
-        Result<List<SpuSaleAttr>> skuSalAttrValues =
-                skuDetailFeignClient.getSkuSalAttrValues(skuId, skuInfo.getSpuId());
-        detailTo.setSpuSaleAttrList(skuSalAttrValues.getData());
+        //        3.查询销售属性名和值
+        CompletableFuture<Void> attrAndValueFuture = skuInfoFuture.thenAcceptAsync(skuInfo -> {
+            Result<List<SpuSaleAttr>> skuSalAttrValues =
+                    skuDetailFeignClient.getSkuSalAttrValues(skuId, skuInfo.getSpuId());
+            detailTo.setSpuSaleAttrList(skuSalAttrValues.getData());
+        }, executor);
 //        4.查询商品实时价格
-        Result<BigDecimal> sku1010Price = skuDetailFeignClient.getSku1010Price(skuId);
-        detailTo.setPrice(sku1010Price.getData());
+        CompletableFuture<Void> priceFuture = CompletableFuture.runAsync(() -> {
+            Result<BigDecimal> sku1010Price = skuDetailFeignClient.getSku1010Price(skuId);
+            detailTo.setPrice(sku1010Price.getData());
+        }, executor);
+
 //        5.查sku组合
-        Result<String> skuValueJson = skuDetailFeignClient.
-                getSkuValueJson(skuInfo.getSpuId());
-        detailTo.setValuesSkuJson(skuValueJson.getData());
+        CompletableFuture<Void> skuValueJsonFuture = skuInfoFuture.thenAcceptAsync(skuInfo -> {
+            Result<String> skuValueJson = skuDetailFeignClient.
+                    getSkuValueJson(skuInfo.getSpuId());
+            detailTo.setValuesSkuJson(skuValueJson.getData());
+        }, executor);
 
         //    6.根据三级分类id查询分类
-        Result<CategoryViewTo> categoryView = skuDetailFeignClient
-                .getCategoryView(skuInfo.getCategory3Id());
-        detailTo.setCategoryView(categoryView.getData());
+        CompletableFuture<Void> categoryViewFuture = skuInfoFuture.thenAcceptAsync(skuInfo -> {
+            Result<CategoryViewTo> categoryView = skuDetailFeignClient
+                    .getCategoryView(skuInfo.getCategory3Id());
+            detailTo.setCategoryView(categoryView.getData());
+        }, executor);
+
+         CompletableFuture.allOf(imageFuture,skuValueJsonFuture, priceFuture,
+                 attrAndValueFuture, categoryViewFuture).join();
+
         return detailTo;
     }
 }
--- service/service-item/src/test/java/com/atguigu/gmall/item/AsyncTest.java
+++ service/service-item/src/test/java/com/atguigu/gmall/item/AsyncTest.java
@@ -2,12 +2,119 @@ package com.atguigu.gmall.item;
 
 
 
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.*;
 
 public class AsyncTest {
 
+   static ExecutorService executor = Executors.newFixedThreadPool(4);
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
+//        多任务组合
+        CompletableFuture<Void> future1 = CompletableFuture.runAsync(() -> {
+            System.out.println("aaa");
+        });
+
+        CompletableFuture<Integer> future2 = CompletableFuture.supplyAsync(() -> {
+            return 222;
+        });
+
+
+        CompletableFuture<Void> future3 = CompletableFuture.runAsync(() -> {
+            System.out.println("bbb");
+        });
+
+//        CompletableFuture.allOf(future1,future2,future3).thenRun(()->{
+//            System.out.println("都完了========");
+//        });
+
+        CompletableFuture<Void> all = CompletableFuture.allOf(future1, future2, future3);
+
+        all.get();
+
+        System.out.println("hhhh");
+
+        Thread.sleep(10000L);
+    }
+    public static void thenXXX(String[] args) throws InterruptedException {
+//        先计算1+1 算出结果再 执行+1
+//        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+//            System.out.println(Thread.currentThread().getName() + ":计算1+1");
+//            return 2;
+//        }, executor);
+//
+//       future.thenRunAsync(()->{
+//           System.out.println(Thread.currentThread().getName()+"呵呵");
+//       },executor);
+
+//        future.thenAcceptAsync((t)->{
+//            System.out.println(Thread.currentThread().getName()+"接到的t是"+t);
+//            System.out.println(t+3);
+//        },executor);
+
+//        future.thenApplyAsync((t)->{
+//            System.out.println(Thread.currentThread().getName()+"接到的t是"+t);
+//            return t+2;
+//        },executor);
+
+        CompletableFuture.supplyAsync(()->{
+            return 5;
+        }).thenApplyAsync((t)->{
+            return t+3;
+        }).thenApply((t)->{
+            return t*5;
+        }).thenAcceptAsync((t)->{
+            System.out.println("把"+t+"保存到数据库");
+        }).whenComplete((t ,u)->{
+            if (u!=null){
+//                记录日志
+            }
+            System.out.println("执行结束，保存日志");
+        });
+
+
+        Thread.sleep(10000L);
+    }
+
+    public static void starAsync(String[] args) throws Exception {
+        /*
+        *   //1、启动异步任务
+        // runAsync();
+        //    1）、CompletableFuture.runAsync(Runnable) 返回 CompletableFuture<Void>;
+        //             使用默认线程池(ForkJoinPool)，启动一个Runnable任务进行执行，没有返回结果
+        //    2）、CompletableFuture.runAsync(Runnable,指定线程池) 返回 CompletableFuture<Void>;
+        //             使用指定线程池，启动一个Runnable任务进行执行，没有返回结果
+        // supplyAsync();
+        //    1）、CompletableFuture.supplyAsync(Runnable) 返回 CompletableFuture<Integer>
+        //             使用默认线程池(ForkJoinPool)，启动一个Runnable任务进行执行，有返回结果
+        //    2）、CompletableFuture.supplyAsync(Runnable,指定线程池)
+        //             使用指定线程池，启动一个Runnable任务进行执行，有返回结果
+        * */
+
+        System.out.println(Thread.currentThread().getName()+":主线程开始");
+
+//        CompletableFuture.runAsync(()->{
+//            System.out.println(Thread.currentThread().getName() + ":" + UUID.randomUUID().toString());
+//            System.out.println("aa");
+//        },executor);
+
+
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread().getName() + "呵呵");
+            return 2;
+        },executor);
+
+        Integer integer = future.get();//阻塞等待异步结果
+        Integer integer1 = future.get(1, TimeUnit.SECONDS);//限时等待
+
+        System.out.println("异步结果"+integer);
+        System.out.println("异步结果"+integer1);
+
+        Thread.sleep(10000L);
+    }
+
+
+
+    public static void jieshao(String[] args) throws InterruptedException {
 
         System.out.println(Thread.currentThread().getName() + "：呵呵");
         CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
