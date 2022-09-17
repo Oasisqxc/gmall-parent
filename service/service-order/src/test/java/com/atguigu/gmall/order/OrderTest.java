af22e7a1ade4d3e33276d5ae1e7745fda725d1e8
Oasisqxc <qxc3070601292@gmail.com>
2022-09-17 23:58:35 +0800

    day16雪花算法及分库分表读写分离完成

--- service/service-order/src/test/java/com/atguigu/gmall/order/OrderTest.java
+++ service/service-order/src/test/java/com/atguigu/gmall/order/OrderTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
+import java.util.List;
 
 @SpringBootTest
 public class OrderTest {
@@ -18,5 +20,36 @@ public class OrderTest {
         OrderInfo orderInfo = orderInfoMapper.selectById(205);
         System.out.println("orderInfo = " + orderInfo);
 
+    }
+
+    @Test
+    public void testSharding(){
+        OrderInfo info = new OrderInfo();
+        info.setTotalAmount(new BigDecimal("777"));
+        info.setUserId(1L);
+        orderInfoMapper.insert(info);
+
+
+        System.out.println("1号用户订单插入完成....去 1库1表找");
+
+
+        OrderInfo info2 = new OrderInfo();
+        info2.setTotalAmount(new BigDecimal("666"));
+        info2.setUserId(2L);
+        orderInfoMapper.insert(info2);
+        System.out.println("2号用户订单插入完成....去 0库2表找");
+
+        //
+
+    }
+    /**
+     * 不带分片键的查询会全库全表都查询，然后结果归并
+     */
+    @Test
+    public void testALl(){
+        List<OrderInfo> infos = orderInfoMapper.selectList(null);
+        for (OrderInfo info : infos) {
+            System.out.println(info.getId()+"==>"+info.getTotalAmount()+"== user:"+info.getUserId());
+        }
     }
 }
